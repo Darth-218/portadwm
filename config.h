f67af5b8d89aaf20b5e0c74f7183cda003aa103a
darth-218 <yahia.hany.gaber@proton.me>
2024-05-17 18:36:04 +0300

    Changed colors, again

--- config.h
+++ config.h
@@ -18,13 +18,14 @@ static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
+static const char col_blue[] = "#80a0ff";
 static const char col_green[] = "#03A062";
 static const char col_red[] = "#e46876";
 static const char col_black[] = "#000000";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray4, col_black, col_gray1},
-    [SchemeSel] = {col_red, col_black, col_red},
+    [SchemeSel] = {col_blue, col_black, col_blue},
 };
 
 /* tagging */
